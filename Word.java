9b31170ce103360713047c7e2fa6a9b7ec7073ee
Neko Null <jerrylu.mail@qq.com>
2019-03-21 10:01:16 +0800

    Update Word.java

--- Word.java
+++ Word.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 public class Word {
     public String c;
     public boolean b;
@@ -10,5 +12,29 @@ public class Word {
     @Override
     public String toString() {
         return c+"="+(b?"T":"F");
+    }
+
+    public  Word and(Word j){
+        return new Word("("+this.c+"∧"+j.c+")",this.b&&j.b);
+    }
+
+    public  Word or(Word j){
+        return new Word("("+this.c+"∨"+j.c+")",this.b||j.b);
+    }
+
+    public  Word not(){
+        return new Word("("+"¬"+this.c+")",!this.b);
+    }
+
+    public  Word xor(Word j){
+        return new Word("("+this.c+"⊕"+j.c+")",(this.b&&!j.b)||(!this.b&&j.b));
+    }
+
+    public  Word imp(Word j){
+        return new Word("("+this.c+"→"+j.c+")",!(this.b&&!j.b));
+    }
+
+    public  Word equ(Word j){
+        return new Word("("+this.c+"↔"+j.c+")",(this.b&&j.b)||(!this.b&&!j.b));
     }
 }
